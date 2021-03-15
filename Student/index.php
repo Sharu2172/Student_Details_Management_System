e5be47aa08b5dd849e5d7d18bca74116eb57bc4b
Sharu2172 <57060263+Sharu2172@users.noreply.github.com>
2021-03-15 10:05:03 +0530

    Delete Student directory

--- Student/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-   echo '
-   <form method="POST" id="form_id" action="sdashboard/"></form>
-   <script type="text/javascript">
-	   document.forms[\'form_id\'].submit();
-   </script>
-   ';
-   ?>
