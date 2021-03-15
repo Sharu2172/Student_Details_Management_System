9189c58986d9cdc877ca7b04a15d58dc4ec0e300
Sharu2172 <57060263+Sharu2172@users.noreply.github.com>
2021-03-15 10:04:52 +0530

    Delete Admin directory

--- Admin/extra/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-include("Session.php");
-//To redirect if accessed by mistake.
-echo location("../dashboard/");
-?>
--- Admin/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-   echo '
-   <form method="POST" id="form_id" action="dashboard/"></form>
-   <script type="text/javascript">
-	   document.forms[\'form_id\'].submit();
-   </script>
-   ';
-?>
